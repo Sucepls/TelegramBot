5535fcecee978dc6dac9341c54b6aba53c924984
KimiyaPoozesh <86666504+KimiyaPoozesh@users.noreply.github.com>
2022-08-29 19:56:49 +0430

    Added files
    -get quote function 
    -start and help command of the bot

---
+++ TelegramBot.py
@@ -0,0 +1,30 @@
+from telegram.ext import *
+
+import requests
+import json
+
+APIKEY = '5300118748:AAFSwWl0OpbeoYYdjUSJriXEnS08kG8-cPQ'
+
+def get_quote():
+    response = requests.get("https://zenquotes.io/api/random")
+    json_data = json.loads(response.text)
+    quote = json_data[0]['q'] + " --" +  json_data[0]['a']
+    print(quote)
+    return quote
+    
+def start_command(update, context):
+    return update.message.reply_text("hello!\n i will send you a quote evryday at 12 till you die :D!!!")
+
+def help_command(update,context):
+    return update.message.reply_text("And what?\n need to call your mommy?")
+
+updater = Updater(APIKEY, use_context=True)
+
+dp = updater.dispatcher
+
+dp.add_handler(CommandHandler("start", start_command))
+dp.add_handler(CommandHandler("help", help_command))
+get_quote()
+updater.start_polling(2)
+
+updater.idle()
